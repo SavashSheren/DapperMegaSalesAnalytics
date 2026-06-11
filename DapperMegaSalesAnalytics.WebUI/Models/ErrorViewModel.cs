09e6b60ca91632b374dbc1f0d8eca01ccbcabd2b
SAVAS SEREN <sava.se6565@gmail.com>
2026-06-11 05:16:26 +0300

    Add project files.

---
+++ DapperMegaSalesAnalytics.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DapperMegaSalesAnalytics.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
